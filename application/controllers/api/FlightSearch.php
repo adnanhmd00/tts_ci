1fe6d84735c30b6cf426875b8f26068dcc5cbc0c
monu-droom <monudah96@gmail.com>
2021-10-21 12:06:15 +0530

    Changes to Flight Api

--- application/controllers/api/FlightSearch.php
+++ application/controllers/api/FlightSearch.php
@@ -32,12 +32,39 @@ class FlightSearch extends REST_Controller {
     public function index_post()
     {
       $inputs = $this->input->post();
+      $data = [
+        'AdultCount'            => $this->input->post('AdultCount'),
+        'ChildCount'            => $this->input->post('ChildCount'),
+        'ClientId'           => $this->input->post('ClientId'),
+        'EndUserIp'           => $this->input->post('EndUserIp'),
+        'InfantCount'           => $this->input->post('InfantCount'),
+        'JourneyType'           => $this->input->post('JourneyType'),
+        'Password'           => $this->input->post('Password'),
+        'Segments'           => [
+          [
+            'Destination' => $this->input->post('Destination_1'),
+            'FlightCabinClass' => $this->input->post('FlightCabinClass_1'),
+            'Origin' => $this->input->post('Origin_1'),
+            'PreferredArrivalTime' => $this->input->post('PreferredArrivalTime_1'),
+            'PreferredDepartureTime' => $this->input->post('PreferredDepartureTime_1'),
+          ],
+          [
+            'Destination' => $this->input->post('Destination_2'),
+            'FlightCabinClass' => $this->input->post('FlightCabinClass_2'),
+            'Origin' => $this->input->post('Origin_2'),
+            'PreferredArrivalTime' => $this->input->post('PreferredArrivalTime_2'),
+            'PreferredDepartureTime' => $this->input->post('PreferredDepartureTime_2'),
+          ]
+        ],
+        'UserName' => $this->input->post('UserName')
+      ];
+
       /* API URL */
        $url = 'https://flight.srdvtest.com/v5/rest/Search';
        /* Init cURL resource */
        $ch = curl_init($url);;
        /* pass encoded JSON string to the POST fields */
-       curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($inputs));
+       curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
        curl_setopt($ch, CURLOPT_POST, 1);
        // curl_setopt($ch, CURLOPT_USERPWD, "api-key: rzp_test_1U3fdUZx6lMZ13-iCS5tAavgvQ7B9mlv5EZFPio");
        // curl_setopt($ch, CURLOPT_USERPWD, 'rzp_test_1U3fdUZx6lMZ13' . ':' . 'iCS5tAavgvQ7B9mlv5EZFPio');
