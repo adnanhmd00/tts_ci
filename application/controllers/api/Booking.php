412c4156a59a9cc86a16a80d8cf6c49981f55b8a
monu-droom <monudah96@gmail.com>
2021-11-15 14:19:50 +0530

    Order and booking Done!

--- application/controllers/api/Booking.php
+++ application/controllers/api/Booking.php
@@ -23,7 +23,7 @@ class Booking extends REST_Controller {
 	public function index_get($id = 0)
 	{
         if(!empty($id)){
-            $data = $this->db->get_where("bookings", ['id' => $id])->row_array();
+            $data = $this->db->get_where("bookings", ['user_id' => $id])->row_array();
         }else{
             $data = $this->db->get("bookings")->result();
         }
@@ -38,34 +38,61 @@ class Booking extends REST_Controller {
     public function index_post()
     {
         $this->load->helper('string');
-        $inputs = $this->input->post();
+        $apiKey = $this->input->post('API-Token');
         $data = [
-          'user_id'            => $this->input->post('user_id'),
-          'amount'            => $this->input->post('amount'),
-          'flight_pnr'           => $this->input->post('flight_pnr'),
-          'flight_booking_id'           => $this->input->post('flight_booking_id'),
-          'flight_name'           => $this->input->post('flight_name'),
-          'payment_type'           => $this->input->post('payment_type'),
-          'payment_status'           => $this->input->post('payment_status'),
-          'payment_details'           => $this->input->post('payment_details'),
+          'ClientId'                => $this->input->post('ClientId'),
+          'EndUserIp'           => $this->input->post('EndUserIp'),
+          'UserName'          => $this->input->post('UserName'),
+          'Password'           => $this->input->post('Password'),
+          'SrdvType'            => $this->input->post('SrdvType'),
+          'SrdvIndex'           => $this->input->post('SrdvIndex'),
+          'TraceId'               => $this->input->post('TraceId'),
+          'ResultIndex'        => $this->input->post('ResultIndex'),
+          'Passengers'        => [
+            [
+              'Title'                 => $this->input->post('Title'),
+              'FirstName'      => $this->input->post('FirstName'),
+              'LastName'      => $this->input->post('LastName'),
+              'PaxType'         => $this->input->post('PaxType'),
+              'DateOfBirth'   => $this->input->post('DateOfBirth'),
+              'Gender'           => $this->input->post('Gender'),
+              'PassportNo'   => $this->input->post('PassportNo'),
+              'PassportExpiry' => $this->input->post('PassportExpiry'),
+              'AddressLine1' => $this->input->post('AddressLine1'),
+              'City'                   => $this->input->post('City'),
+              'CountryCode'  => $this->input->post('CountryCode'),
+              'CountryName' => $this->input->post('CountryName'),
+              'ContactNo'      => $this->input->post('ContactNo'),
+              'Email'               => $this->input->post('Email'),
+              'IsLeadPax'      => $this->input->post('IsLeadPax'),
+              'Fare' => [
+                'BaseFare'      => $this->input->post('BaseFare'),
+                'Tax'                => $this->input->post('Tax'),
+                'TransactionFee' => $this->input->post('TransactionFee'),
+                'YQTax'                 => $this->input->post('YQTax'),
+                'AdditionalTxnFeeOfrd' => $this->input->post('AdditionalTxnFeeOfrd'),
+                'AdditionalTxnFeePub' => $this->input->post('AdditionalTxnFeePub'),
+                'AirTransFee' => $this->input->post('AirTransFee')
+              ],
+            ],
+          ],
         ];
-        $this->db->insert('bookings',$data);
-        $id = $this->db->insert_id();
-        $amount = $this->input->post('amount');
-          /* API URL */
-         $url = 'https://api.razorpay.com/v1/orders';
+        /* API URL */
+         $url = 'https://flight.srdvtest.com/v5/rest/TicketLCC';
          /* Init cURL resource */
-         $ch = curl_init($url);
-         /* Array Parameter Data */
-         $data = ['amount'=> $amount, 'currency'=>'INR'];
+         $ch = curl_init($url);;
          /* pass encoded JSON string to the POST fields */
          curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
          curl_setopt($ch, CURLOPT_POST, 1);
          // curl_setopt($ch, CURLOPT_USERPWD, "api-key: rzp_test_1U3fdUZx6lMZ13-iCS5tAavgvQ7B9mlv5EZFPio");
-         curl_setopt($ch, CURLOPT_USERPWD, 'rzp_test_1U3fdUZx6lMZ13' . ':' . 'iCS5tAavgvQ7B9mlv5EZFPio');
-        $headers = array();
-        $headers[] = 'Accept: application/json';
-        $headers[] = 'Content-Type: application/json';
+         // curl_setopt($ch, CURLOPT_USERPWD, 'rzp_test_1U3fdUZx6lMZ13' . ':' . 'iCS5tAavgvQ7B9mlv5EZFPio');
+        $headers = array(
+          'Content-Type: application/json',
+          'Accept: application/json',
+          'API-Token:' .$apiKey
+        );
+        // $headers[] = 'Accept: application/json';
+        // $headers[] = 'Content-Type: application/json';
          /* set the content type json */
         curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 
@@ -76,18 +103,27 @@ class Booking extends REST_Controller {
          $result = curl_exec($ch);
          // $result = explode(',', $result);
          // $result = strip_slashes($result);
-         $result = strip_quotes($result);
-         $result = str_replace(array('{','}'), '', $result);
-         $result = explode(',', $result);
-         $result = str_replace('id:', '', $result);
           if (curl_errno($ch)) {
               echo 'Error:' . curl_error($ch);
           }
-
          /* close cURL resource */
-         curl_close($ch);
+          curl_close($ch);
+          $result = json_decode($result, true);
+          print_r($result);
+          $data = [
+            'user_id'            => $this->input->post('user_id'),
+            'amount'            => $this->input->post('amount'),
+            'flight_pnr'           => $result['PNR'],
+            'flight_booking_id'           => $result['BookingId'],
+            'flight_name'           => $result['AirlineName'],
+            'payment_type'           => $this->input->post('payment_type'),
+            'payment_status'           => $this->input->post('payment_status'),
+            'payment_details'           => $this->input->post('payment_details'),
+          ];
+          $this->db->insert('bookings',$data);
+          $id = $this->db->insert_id();
 
-          $this->response(['status' => 'success','boooking_id' => $id, 'order_id' => $result[0], 'message' => 'A new Booking Created Successfully.'], REST_Controller::HTTP_OK);
+        $this->response(['status' => 'success','booking_id' => $id, 'booking_status' => $result['Status'], 'data' =>$result , 'message' => 'A new Booking Created Successfully.'], REST_Controller::HTTP_OK);
       }
 
     /**
