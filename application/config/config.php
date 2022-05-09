b696f580011c5460fce65347ae05246666c147a5
thetravelsquare <server.thetravelsquare@gmail.com>
2022-05-09 13:10:08 +0530

    policy added and coupons also

--- application/config/config.php
+++ application/config/config.php
@@ -24,8 +24,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 // $config['base_url'] = 'http://ts.frantic.in/';
-$config['base_url'] = "http://partner.thetravelsquare.in/";
-// $config['base_url'] = 'http://localhost:5000/';
+// $config['base_url'] = "http://partner.thetravelsquare.in/";
+$config['base_url'] = 'http://localtts.in';
 /*
 |--------------------------------------------------------------------------
 | Index File
---
+++ application/controllers/api/Baggage.php
@@ -0,0 +1,35 @@
+<?php
+
+   require APPPATH . '/libraries/REST_Controller.php';
+   use Restserver\Libraries\REST_Controller;
+
+class Baggage extends REST_Controller {
+
+	  /**
+     * Get All Data from this method.
+     *
+     * @return Response
+    */
+    public function __construct() {
+       parent::__construct();
+       $this->load->database();
+    }
+
+    /**
+     * Get All Data from this method.
+     *
+     * @return Response
+    */
+	public function index_get()
+	{
+      $data = $this->db->get_where("tts_baggage")->result();
+      $this->response($data, REST_Controller::HTTP_OK);
+	}
+
+    /**
+     * Get All Data from this method.
+     *
+     * @return Response
+    */
+
+}
